06c1a9c1a9398d7a483e4480ffcb3c40e93fdf66
ahmedshuaib <ahmedshuaib46@gmail.com>
2021-05-17 22:20:32 +0600

    added admin route prefix

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::namespace(TFMSoftware\DhruFusion\Http\Controllers::class)->group(function() {
+Route::namespace(TFMSoftware\DhruFusion\Http\Controllers::class)->prefix('admin')->group(function() {
     //api key genrate and modify here
     Route::apiResource('/dhru/api/key', ApiKeyController::class);
 });
