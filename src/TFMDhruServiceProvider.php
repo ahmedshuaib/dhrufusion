931bca4fbf1273b5f6899192dcbf083a77f5014c
ahmedshuaib <ahmedshuaib46@gmail.com>
2021-05-17 22:23:57 +0600

    added rotues for publish vendor

--- src/TFMDhruServiceProvider.php
+++ src/TFMDhruServiceProvider.php
@@ -28,7 +28,8 @@ class TFMDhruServiceProvider extends ServiceProvider
 
         $this->publishes([
             __DIR__ . '/Http/Controllers/ApiKeyController.php' => 'Dhru/Http/Controllers/ApiKeyController.php',
-            __DIR__ . '/dhru/index.php' => 'public/api/dhru/index.php'
+            __DIR__ . '/dhru/index.php' => 'public/api/dhru/index.php',
+            __DIR__ . '/routes/web.php' => 'Dhru/routes/web.php',
         ]);
     }
 }
