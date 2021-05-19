6b466013aa26664993821d14724a6d075eba23f4
ahmedshuaib <ahmedshuaib46@gmail.com>
2021-05-19 11:36:36 +0600

    modified middleware

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Route;
 
 use TFMSoftware\DhruFusion\Http\Controllers;
@@ -8,15 +9,16 @@ use TFMSoftware\DhruFusion\Http\Controllers\DhruApiController;
 Route::namespace(Controllers::class)->prefix('admin')
 ->middleware(['web', 'auth:admin', 'admin', 'verified'])->group(function () {
     //api key genrate and modify here
-
-    Route::prefix('dhru/api')->group(function() {
-
-        Route::apiResource('key', ApiKeyController::class);
-
-        Route::post('login',  'DhruApiController@dhru_login');
-        Route::post('account', 'DhruApiController@account_info');
-        Route::post('order_license', 'DhruApiController@license_order');
-
-    });
+    Route::apiResource('/dhru/api/key', ApiKeyController::class);
 
 });
+
+
+Route::post('login',  'DhruApiController@dhru_login')->middleware(['api', 'guest:admin']);
+
+Route::namespace(Controller::class)->prefix('/dhru')
+->middleware(['api', 'auth:admin', 'admin', 'verified'])->group(function() {
+    Route::post('account', 'DhruApiController@account_info');
+    Route::post('order_license', 'DhruApiController@license_order');
+});
+
